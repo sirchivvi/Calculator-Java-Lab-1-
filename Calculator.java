a3fd89fb45f36125165e05cce1956710742c1617
sirchivvi <137323896+sirchivvi@users.noreply.github.com>
2025-02-01 23:14:35 +0530

    Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,41 +1,42 @@
+
 // Name: Akhil Chivukula
 // PRN: 23070126009
 // Batch: AIML - A1
 
 class Calculator {
-    int[] num; // Declare num array
+    int[] num; // Stores two numbers for operations
 
     // Constructor to initialize numbers
     public Calculator() {
         UserInput in = new UserInput();
-        this.num = in.getTwoNumbers(); // Take input only once
+        this.num = in.getTwoNumbers(); // Takes input once and stores in num
     }
 
-    // Addition
+    // Function to perform addition
     int addition() {
         return num[0] + num[1];
     }
 
-    // Subtraction
+    // Function to perform subtraction
     int subtraction() {
         return num[0] - num[1];
     }
 
-    // Multiplication
+    // Function to perform multiplication
     int multiplication() {
         return num[0] * num[1];
     }
 
-    // Division
+    // Function to perform division (handling division by zero)
     double division() {
-        if (num[1] == 0) { // Check for division by zero
+        if (num[1] == 0) { 
             System.out.println("Division by zero is not allowed. Returning 0 as the result.");
-            return 0; // Return 0 or any default value
+            return 0;
         }
         return (double) num[0] / num[1];
     }
 
-    // Fibonacci sequence
+    // Function to generate Fibonacci sequence up to n terms
     void fibonacci(int n) {
         int a = 0, b = 1, sum;
         System.out.print("Fibonacci sequence: ");
@@ -50,7 +51,7 @@ class Calculator {
         System.out.println();
     }
 
-    // Mean of array
+    // Function to calculate the mean of an array
     void mean(int[] array) {
         double sum = 0;
 
@@ -61,7 +62,7 @@ class Calculator {
         System.out.println("Mean is: " + (sum / array.length));
     }
 
-    // Variance of array
+    // Function to calculate the variance of an array
     void variance(int[] array) {
         double sum = 0;
 
@@ -78,4 +79,14 @@ class Calculator {
 
         System.out.println("Variance is: " + (sumSquaredDiffs / array.length));
     }
-}
+
+    // Function to calculate the standard deviation of an array
+    void standardDeviation(int[] array) {
+        double sum = 0;
+
+        for (int num : array) {
+            sum += num;
+        }
+
+        double mean = sum / array.length;
+        doub
